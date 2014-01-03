54dc142ae5bcb9f033913d78feeedfccdefbda68
Xu Hao <xuhao3e8@gmail.com>
2014-01-03 21:27:06 +0800

    Initial Commit

---
+++ Rand_Walk/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Rand_Walk
+//
+//  Created by xuhao on 14-1-3.
+//  Copyright (c) 2014年 xuhao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
